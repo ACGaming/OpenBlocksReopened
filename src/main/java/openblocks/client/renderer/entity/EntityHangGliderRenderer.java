92900e19437294f6e00acad1b25fdfd2312f721e
ACGaming <4818419+ACGaming@users.noreply.github.com>
2025-10-19 15:51:32 +0200

    Utilize GlStateManager for hang glider renderer

--- src/main/java/openblocks/client/renderer/entity/EntityHangGliderRenderer.java
+++ src/main/java/openblocks/client/renderer/entity/EntityHangGliderRenderer.java
@@ -9,7 +9,6 @@ import net.minecraft.util.ResourceLocation;
 import openblocks.OpenBlocks;
 import openblocks.common.entity.EntityHangGlider;
 import openmods.renderer.DisplayListWrapper;
-import org.lwjgl.opengl.GL11;
 
 public class EntityHangGliderRenderer extends Render<EntityHangGlider> {
 
@@ -21,23 +20,22 @@ public class EntityHangGliderRenderer extends Render<EntityHangGlider> {
 		public void compile() {
 			GlStateManager.disableCull();
 			GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
-			GL11.glBegin(GL11.GL_QUADS);
+			GlStateManager.glBegin(0x7); // GL_QUADS
 
-			GL11.glTexCoord2f(1, 1);
-			GL11.glVertex3f(QUAD_HALF_SIZE, 0, QUAD_HALF_SIZE);
+			GlStateManager.glTexCoord2f(1, 1);
+			GlStateManager.glVertex3f(QUAD_HALF_SIZE, 0, QUAD_HALF_SIZE);
 
-			GL11.glTexCoord2f(0, 1);
-			GL11.glVertex3f(-QUAD_HALF_SIZE, 0, QUAD_HALF_SIZE);
+			GlStateManager.glTexCoord2f(0, 1);
+			GlStateManager.glVertex3f(-QUAD_HALF_SIZE, 0, QUAD_HALF_SIZE);
 
-			GL11.glTexCoord2f(0, 0);
-			GL11.glVertex3f(-QUAD_HALF_SIZE, 0, -QUAD_HALF_SIZE);
+			GlStateManager.glTexCoord2f(0, 0);
+			GlStateManager.glVertex3f(-QUAD_HALF_SIZE, 0, -QUAD_HALF_SIZE);
 
-			GL11.glTexCoord2f(1, 0);
-			GL11.glVertex3f(QUAD_HALF_SIZE, 0, -QUAD_HALF_SIZE);
+			GlStateManager.glTexCoord2f(1, 0);
+			GlStateManager.glVertex3f(QUAD_HALF_SIZE, 0, -QUAD_HALF_SIZE);
 
-			GL11.glEnd();
+			GlStateManager.glEnd();
 			GlStateManager.enableCull();
-
 		}
 	};
 
@@ -61,31 +59,31 @@ public class EntityHangGliderRenderer extends Render<EntityHangGlider> {
 
 		final float rotation = interpolateRotation(glider.prevRotationYaw, glider.rotationYaw, f1);
 
-		GL11.glPushMatrix();
+		GlStateManager.pushMatrix();
 
-		GL11.glTranslated(x, y, z);
-		GL11.glRotatef(180.0F - rotation, 0.0F, 1.0F, 0.0F);
+		GlStateManager.translate(x, y, z);
+		GlStateManager.rotate(180.0F - rotation, 0.0F, 1.0F, 0.0F);
 		if (isLocalPlayer && isFpp) {
 			// move over head when flying in FPP
-			GL11.glTranslated(0, +0.7, 0);
+			GlStateManager.translate(0, +0.7, 0);
 		} else {
 			if (!isDeployed) {
 				// move up little bit
-				GL11.glTranslated(0, +0.2, +0.3);
+				GlStateManager.translate(0, +0.2, +0.3);
 			} else {
 				// move closer to back and forward when flying
-				GL11.glTranslated(0, -0.5, -1.0);
+				GlStateManager.translate(0, -0.5, -1.0);
 			}
 		}
 
 		if (!isDeployed) {
-			GL11.glRotatef(ONGROUND_ROTATION, 1, 0, 0);
-			GL11.glScalef(0.4f, 1f, 0.4f);
+			GlStateManager.rotate(ONGROUND_ROTATION, 1, 0, 0);
+			GlStateManager.scale(0.4f, 1f, 0.4f);
 		}
 
 		bindTexture(texture);
 		gliderRender.render();
-		GL11.glPopMatrix();
+		GlStateManager.popMatrix();
 	}
 
 	private static float interpolateRotation(float prevRotation, float nextRotation, float modifier) {
